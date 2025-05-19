b8b6ead5261167d16d0c052b7f3f4b93d40d9e76
rowing0328 <dev.hyoseung@gmail.com>
2025-05-19 16:20:37 +0900

    ✨ 새 기능 : React 기본 예제 HTML 및 버튼 컴포넌트 추가 - html/sample.html: React·ReactDOM CDN 로드 및 <MyButton> 렌더링 샘플 - js/my-button.js: 간단한 MyButton 컴포넌트 구현
    리액트 기초 개념 학습용 최소 예제 제공

---
+++ javascript/react-basics/js/my-button.js
@@ -0,0 +1,11 @@
+function myButton() {
+    const [isClicked, setIsClicked] = React.useState(false);
+
+    return React.createElement(
+        "button",
+        { onClick: () => setIsClicked(!isClicked) },
+        isClicked ? "클릭되었습니다." : "클릭해 주세요.");
+
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    root.render(React.createElement(myButton));
+}
